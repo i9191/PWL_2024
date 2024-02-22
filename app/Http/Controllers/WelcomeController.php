f9d63dead5046adaee1175610d409db609779fa1
Ibnu Tsalis Assalam <ibnutsalisassalam@gmail.com>
2024-02-22 16:49:18 +0700

    JS2 - Meneruskan data ke view

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,6 +11,8 @@ class WelcomeController extends Controller
         return 'Hello World';
     }
     public function greeting(){
-        return view('blog.hello', ['name' => 'Ibnu Tsalis Assalam']);
+        return view('blog.hello')
+            ->with('name','Ibnu Tsalis Assalam')
+            ->with('occupation','Student');
     }
 }
